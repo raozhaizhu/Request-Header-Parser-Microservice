8b759333d06df79b9eebdbfce6398a4ebddc2299
raozhaizhu <raozhaizhu@gmail.com>
2025-01-30 15:48:01 +0800

    fix

--- index.js
+++ index.js
@@ -16,15 +16,32 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/views/index.html');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+    res.json({ greeting: 'hello API' });
+});
+
+// 对whoami之API的实现
+app.get('/api/whoami', function (req, res) {
+    // 获取用户IP地址,放在ipaddress键下
+    const ipaddress = req.ip || req.headers['x-forwarded-for'];
+    // 获取用户语言信息,放在language键下
+    const language = req.headers['accept-language'].split(',')[0];
+    // 获取用户操作系统和浏览器信息,放在software键下
+    const software = req.headers['user-agent'];
+    // 组装后,发送响应数据
+    res.json({
+        ipaddress: ipaddress,
+        language: language,
+        software: software,
+    });
 });
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+    console.log('Your app is listening on port ' + listener.address().port);
 });
+
